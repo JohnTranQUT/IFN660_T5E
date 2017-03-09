e091c16cd7a118e15dd1d7fd103dabf276284c69
exchange321 <chuen_hong@yahoo.com>
2017-03-09 19:51:58 +1000

    main.cpp and y.tab.h template for token assessment

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <y.tab.h>
+
+int yylex();
+
+YYSTYPE yylval;
+
+extern FILE *yyin;
+
+using namespace std;
+void main(int argc, char *argv[]) {
+	fopen_s(&yyin, argv[1], "r");
+	int token;
+	do {
+		token = yylex();
+		printf("Next Token: ");
+		switch (token) {
+			case 0: {
+				printf("EOF\n");
+				break;
+			}
+			default: {
+				printf("'%c'\n", token);
+				break;
+			}
+		}
+	} while (token != 0);
+}
---
+++ y.tab.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum yytokentype {
+};
+
+union YYSTYPE {
+};
+
+extern YYSTYPE yylval;
