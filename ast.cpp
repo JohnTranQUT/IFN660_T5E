2de2c2a3f5619da8ea58f3f5346c58117c5f06b0
exchange321 <chuen_hong@yahoo.com>
2017-04-03 20:05:42 +1000

    Implemented universal dump function

--- ast.cpp
+++ ast.cpp
@@ -2,52 +2,142 @@
 #include <ast.h>
 
 using namespace std;
+vector<Node*> Node::history;
+
+void Node::indentation(int indent) {
+	for (auto i = 0; i < indent; i++) {
+		printf("\t");
+	}
+}
 
 Identifier::Identifier(char *_identifier) {
 	identifier = _identifier;
+	history.emplace(history.begin(), this);
 }
 
-void Identifier::dump() {
+void Identifier::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	printf("Identifier (%s)\n", identifier);
+}
+
+int Identifier::getIndent() {
+	return indent;
+}
+
+void Identifier::setIndent(int _indent) {
+	indent = _indent;
 }
 
 DecimalLiteral::DecimalLiteral(double _decimalLiteral) {
 	decimalLiteral = _decimalLiteral;
+	history.emplace(history.begin(), this);
 }
 
-void DecimalLiteral::dump() {
+void DecimalLiteral::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	printf("Decimal Literal (%f)\n", decimalLiteral);
+}
+
+int DecimalLiteral::getIndent() {
+	return indent;
+}
+
+void DecimalLiteral::setIndent(int _indent) {
+	indent = _indent;
 }
 
 AssignmentExpression::AssignmentExpression(Identifier* _identifier, DecimalLiteral* _decimalLiteral) {
 	identifier = _identifier;
 	decimalLiteral = _decimalLiteral;
+	next.push_back(identifier);
+	next.push_back(decimalLiteral);
+	history.emplace(history.begin(), this);
 }
 
-void AssignmentExpression::dump() {
+void AssignmentExpression::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	puts("AssignmentExpression: ");
+	for (auto &i : next) {
+		i->dump(indent + 1);
+	}
+}
+
+int AssignmentExpression::getIndent() {
+	return indent;
+}
+
+void AssignmentExpression::setIndent(int _indent) {
+	indent = _indent;
 }
 
 ExpressionStatement::ExpressionStatement(AssignmentExpression *_assignmentExpression) {
 	assignmentExpression = _assignmentExpression;
+	next.push_back(assignmentExpression);
+	history.emplace(history.begin(), this);
 }
 
-void ExpressionStatement::dump() {
+void ExpressionStatement::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	puts("ExpressionStatement: ");
+	for (auto &i : next) {
+		i->dump(indent + 1);
+	}
+}
+
+int ExpressionStatement::getIndent() {
+	return indent;
+}
+
+void ExpressionStatement::setIndent(int _indent) {
+	indent = _indent;
 }
 
 Statement::Statement(ExpressionStatement *_expressionStatement) {
 	expressionStatement = _expressionStatement;
+	next.push_back(expressionStatement);
+	history.emplace(history.begin(), this);
 }
 
-void Statement::dump() {
+void Statement::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	puts("Statement: ");
+	for (auto &i : next) {
+		i->dump(indent + 1);
+	}
+}
+
+int Statement::getIndent() {
+	return indent;
+}
+
+void Statement::setIndent(int _indent) {
+	indent = _indent;
 }
 
 Script::Script(Statement *_statement) {
 	statement = _statement;
+	next.push_back(statement);
+	history.emplace(history.begin(), this);
 }
 
-void Script::dump() {
+void Script::dump(int currentLevel) {
+	setIndent(currentLevel);
+	indentation(indent);
 	puts("Script: ");
+	for (auto &i : next) {
+		i->dump(indent + 1);
+	}
+}
+
+int Script::getIndent() {
+	return indent;
+}
+
+void Script::setIndent(int _indent) {
+	indent = _indent;
 }
--- ast.h
+++ ast.h
@@ -1,51 +1,81 @@
 #pragma once
 #include <cstdio>
+#include <vector>
 
+using namespace std;
 class Node {
 public:
+	static vector<Node*> history;
 	virtual ~Node() = default;
-	virtual void dump() = 0;
+	virtual void dump(int = 0) = 0;
+	virtual int getIndent() = 0;
+	virtual void setIndent(int) = 0;
+	static void indentation(int);
 };
 
 class Identifier : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	char* identifier;
 public:
 	explicit Identifier(char*);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
 
 class DecimalLiteral : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	double decimalLiteral;
 public:
 	explicit DecimalLiteral(double);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
 
 class AssignmentExpression : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	Identifier* identifier;
 	DecimalLiteral* decimalLiteral;
 public:
 	AssignmentExpression(Identifier*, DecimalLiteral*);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
 
 class ExpressionStatement : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	AssignmentExpression* assignmentExpression;
 public:
 	explicit ExpressionStatement(AssignmentExpression*);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
 
 class Statement : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	ExpressionStatement* expressionStatement;
 public:
 	explicit Statement(ExpressionStatement*);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
 
 class Script : public Node {
+	int indent = 0;
+	vector<Node*> next;
 	Statement* statement;
 public:
 	explicit Script(Statement*);
-	void dump() override;
+	void dump(int = 0) override;
+	int getIndent() override;
+	void setIndent(int) override;
 };
