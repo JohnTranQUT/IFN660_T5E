951e36c972ed80e19b40b23fa8a2e95415ad1063
exchange321 <chuen_hong@yahoo.com>
2017-04-03 19:14:34 +1000

    Initialized main.cpp for ast test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
-#include <parser.h>
+//#include <parser.h>
 
-extern FILE *yyin;
+//extern FILE *yyin;
 
 using namespace std;
 void main(int argc, char *argv[]) {
-	fopen_s(&yyin, argv[1], "r");	
-	yyparse();
+//	fopen_s(&yyin, argv[1], "r");	
+//	yyparse();
 }
