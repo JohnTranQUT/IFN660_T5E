298bc4c93999c8e5caf2f8709a009bedbcde78d8
a98ccp <a98ccp@gmail.com>
2017-05-01 17:51:47 +1000

    Not an intelligent method implemented

--- operator.cpp
+++ operator.cpp
@@ -1,5 +1,39 @@
-#include <Type.h>
+#include "operators.h"
 
-Type *addition(Type * = nullptr, Type * = nullptr) {
-	return new Type();
+static std::string addition(std::string lval, std::string rval) 
+{ return lval + rval; }
+
+template <class T2>
+static std::string addition(std::string lval, T2 rval)
+{
+	auto temp = rval;
+	if (typeid(temp) == typeid(bool)) {
+		if (temp) return lval + std::string("true");
+		else return lval + std::string("false");
+	}
+	return lval + std::to_string(temp);
+}
+
+
+template <class T1>
+static std::string addition(T1 lval, std::string rval)
+{
+	auto temp = lval;
+	std::cout << typeid(temp).name() << "\n";
+	if (typeid(temp) == typeid(bool)) {
+		if (temp) return std::string("true") + rval;
+		else return std::string("false") + rval;
+	}
+	return std::to_string(temp) + rval;
+}
+
+static int addition(int lval, int rval)
+{
+	return lval + rval;
+}
+
+template<class T1, class T2>
+static double addition(T1 lval, T2 rval)
+{
+	return lval + rval;
 }
--- operator.h
+++ operator.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <string>
+
+class Operators {
+public:
+	static std::string addition(std::string, std::string);	
+	template <class T2>
+	static std::string addition(std::string , T2);
+	template <class T1>
+	static std::string addition(T1, std::string);
+	
+	
+	static int addition(int, int);
+
+	template<class T1, class T2>
+	static double addition(T1, T2);
+};
