154fdf8be2039490d4d5b30504b14e0fc0905d58
exchange321 <adchuenhong@gmail.com>
2017-05-02 16:08:32 +1000

    Initialized DeclarativeEnvironmentRecord, GlobalEnvironmentRecord, and ObjectEnvironmentRecord

---
+++ Types/SpecificationTypes/RecordType/EnvironmentRecord/GlobalEnvironmentRecord/GlobalEnvironmentRecord.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Types/SpecificationTypes/RecordType/EnvironmentRecord/EnvironmentRecord.h>
+
+class GlobalEnvironmentRecord : public EnvironmentRecord {
+public:
+	virtual ~GlobalEnvironmentRecord() { }
+};
---
+++ Types/SpecificationTypes/RecordType/EnvironmentRecord/ObjectEnvironmentRecord/ObjectEnvironmentRecord.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Types/SpecificationTypes/RecordType/EnvironmentRecord/EnvironmentRecord.h>
+
+class ObjectEnvironmentRecord : public EnvironmentRecord {
+public:
+	virtual ~ObjectEnvironmentRecord() { }
+};
