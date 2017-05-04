cf766682977d434857f85ad90a3b912371ea5d15
exchange321 <adchuenhong@gmail.com>
2017-05-04 14:25:32 +1000

    ListType now functions properly

--- RuntimeLib/Types/SpecificationTypes/ListType/ListType.h
+++ RuntimeLib/Types/SpecificationTypes/ListType/ListType.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <vector>
 #include <RuntimeLib/Types/SpecificationTypes/SpecificationTypes.h>
+#include <RuntimeLib/Types/LanguageTypes/BooleanType/BooleanType.h>
 
 class ListType : public SpecificationType {
+	vector<Type *> list;
 public:
 	ListType() { }
+	BooleanType *_contain(Type *);
 };
