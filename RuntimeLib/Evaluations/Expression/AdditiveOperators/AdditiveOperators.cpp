a8ca1a9483828fa211170cc7cfbbdbe361e8f76c
exchange321 <adchuenhong@gmail.com>
2017-05-04 12:59:10 +1000

    Refactor AdditionOperator and SubtractionOperator

--- RuntimeLib/Evaluations/Expression/AdditiveOperators/AdditiveOperators.cpp
+++ RuntimeLib/Evaluations/Expression/AdditiveOperators/AdditiveOperators.cpp
@@ -13,8 +13,7 @@ LanguageType *AdditionOperator(LanguageType *lref, LanguageType *rref) {
 	}
 	auto lnum = *ToNumber(lprim);
 	auto rnum = *ToNumber(rprim);
-	auto result = lnum + rnum;
-	return dynamic_cast<LanguageType *>(result);
+	return lnum + rnum;
 }
 
 LanguageType *SubtractionOperator(LanguageType *lref, LanguageType *rref) {
@@ -22,6 +21,5 @@ LanguageType *SubtractionOperator(LanguageType *lref, LanguageType *rref) {
 	auto rval = _ToLanguageType(GetValue(rref));
 	auto lnum = *ToNumber(lval);
 	auto rnum = NumberType(-ToNumber(rval)->_getValue());
-	auto result = lnum + rnum;
-	return dynamic_cast<LanguageType *>(result);
+	return lnum + rnum;
 }
