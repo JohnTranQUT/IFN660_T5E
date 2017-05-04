d1247529bc2d56d4f0d4ecaea7c46ea1938edf00
John Tran <j23.tran@connect.qut.edu.au>
2017-05-04 17:16:08 +1000

    Test attempt

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <RuntimeLib\Type\UndefinedType\UndefinedType.h>
 
 #include <RuntimeLib\Expressions\AdditiveOperators.h>
+#include <string>
 
 //#include <parser.h>
 
@@ -16,6 +17,10 @@
 using namespace std;
 
 void main(int argc, char *argv[]) {
+	auto temp = additiveOperator(new NumberType(1), new NumberType(2));
+	auto _temp = dynamic_cast<NumberType *>(temp);
+	puts(to_string(_temp->_getValue()));
+	puts(_temp->_getType());
 //	fopen_s(&yyin, argv[1], "r");
 //	yyparse();
 //	addition();
