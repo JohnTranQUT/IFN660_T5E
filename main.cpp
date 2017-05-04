0228523bf427dbd0d0047cf9b0b4fdf950d8af66
exchange321 <adchuenhong@gmail.com>
2017-05-04 15:28:18 +1000

    Added tests for Infinity/NaN arithmetic

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#include <limits>
 #include <RuntimeLib/Types/LanguageTypes/LanguageTypesFunc.h>
 #include <RuntimeLib/Types/LanguageTypes/ObjectType/Objects/ObjectConstructor.h>
 #include <RuntimeLib/_Helpers/_Helpers.h>
@@ -18,7 +19,7 @@ void main(int argc, char *argv[]) {
 #endif
 
 #ifdef SA
-	puts("\nBasic LanguageTypes Demo\n");
+	puts("\nBasic LanguageTypes Test\n");
 
 	_calculate(new StringType("Hello"), "+", new NullType());
 	_calculate(new NumberType(50), "-", new BooleanType(false));
@@ -26,13 +27,49 @@ void main(int argc, char *argv[]) {
 	_calculate(new BooleanType(true), "/", new StringType("1000"));
 	_calculate(new StringType("20"), "%", new NumberType(5));
 
-	puts("\nObjectType/Objects Demo\n");
+	puts("\nObjectType/Objects Test\n");
 
 	_calculate(String(new StringType("World")), "+", Object());
 	_calculate(Object(new BooleanType(false)), "-", Number(new NumberType(20)));
 	_calculate(Object(new NumberType(5)), "*", Object(new StringType("2")));
 	_calculate(String(new StringType("30")), "/", Object(Object(Object(Boolean(new BooleanType(true))))));
 	_calculate(Boolean(new BooleanType(true)), "%", String(new StringType("3")));
+
+	puts("\nInfinity/NaN Test\n");
+
+	puts("+\n");
+	_calculate(new NumberType(NAN), "+", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "+", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "+", new NumberType(-INFINITY));
+	_calculate(new NumberType(-INFINITY), "+", new NumberType(-INFINITY));
+	_calculate(new NumberType(INFINITY), "+", new BooleanType(true));
+
+	puts("\n-\n");
+	_calculate(new NumberType(NAN), "-", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "-", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "-", new NumberType(-INFINITY));
+	_calculate(new NumberType(-INFINITY), "-", new NumberType(-INFINITY));
+	_calculate(new NumberType(-INFINITY), "-", new BooleanType(true));
+
+	puts("\n*\n");
+	_calculate(new NumberType(NAN), "*", new NumberType(NAN));
+	_calculate(new NumberType(NAN), "*", new BooleanType(true));
+	_calculate(new NumberType(INFINITY), "*", new BooleanType(false));
+	_calculate(new NumberType(INFINITY), "*", new NumberType(-INFINITY));
+
+	puts("\n/\n");
+	_calculate(new NumberType(NAN), "/", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "/", new NumberType(INFINITY));
+	_calculate(new NumberType(INFINITY), "/", new BooleanType(true));
+	_calculate(new BooleanType(true), "/", new NumberType(INFINITY));
+	_calculate(new BooleanType(false), "/", new BooleanType(false));
+	_calculate(new BooleanType(true), "/", new BooleanType(false));
+
+	puts("\n%\n");
+	_calculate(new NumberType(NAN), "%", new NumberType(NAN));
+	_calculate(new NumberType(INFINITY), "%", new BooleanType(false));
+	_calculate(new BooleanType(false), "%", new NumberType(-INFINITY));
+	_calculate(new BooleanType(false), "%", new BooleanType(true));
 
 	puts("");
 #endif
