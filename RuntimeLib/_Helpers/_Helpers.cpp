e5e31f59b1abb3eba06b0403ca9648833e7d2178
exchange321 <adchuenhong@gmail.com>
2017-05-09 22:01:03 +1000

    Implemented _listItemsInRecord()

--- RuntimeLib/_Helpers/_Helpers.cpp
+++ RuntimeLib/_Helpers/_Helpers.cpp
@@ -1,4 +1,8 @@
 #include <RuntimeLib/_Helpers/_Helpers.h>
+#include <RuntimeLib/Types/SpecificationTypes/RecordType/CompletionType/CompletionType.h>
+#include <RuntimeLib/Types/SpecificationTypes/RecordType/EnvironmentRecord/DeclarativeEnvironmentRecord/DeclarativeEnvironmentRecord.h>
+#include <RuntimeLib/Types/SpecificationTypes/RecordType/EnvironmentRecord/ObjectEnvironmentRecord/ObjectEnvironmentRecord.h>
+#include <RuntimeLib/Types/SpecificationTypes/RecordType/EnvironmentRecord/GlobalEnvironmentRecord/GlobalEnvironmentRecord.h>
 
 void _calculate(LanguageType *lhs, string op, LanguageType *rhs) {
 	LanguageType *result = nullptr;
@@ -22,5 +26,40 @@ void _calculate(LanguageType *lhs, string op, LanguageType *rhs) {
 	} else {
 		puts("calculate");
 		exit(0);
+	}
+}
+
+void _listItemsInRecord(RecordType *record) {
+	if (auto _record = dynamic_cast<CompletionType *>(record)) {
+		auto items = _record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
+	} else if (auto _record = dynamic_cast<DeclarativeEnvironmentRecord *>(record)) {
+		auto items = _record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
+	} else if (auto _record = dynamic_cast<ObjectEnvironmentRecord *>(record)) {
+		auto items = _record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
+	} else if (auto _record = dynamic_cast<GlobalEnvironmentRecord *>(record)) {
+		auto items = _record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
+	} else if (auto _record = dynamic_cast<EnvironmentRecord *>(record)) {
+		auto items = _record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
+	} else {
+		auto items = record->_getValue();
+		for (auto it = items.begin(); it != items.end(); ++it) {
+			auto items = _record->_getValue();
+			cout << it->first << " -> " << ToString(dynamic_cast<LanguageType *>(it->second))->_getValue() << endl;
+		}
 	}
 }
--- RuntimeLib/_Helpers/_Helpers.h
+++ RuntimeLib/_Helpers/_Helpers.h
@@ -4,5 +4,7 @@
 #include <RuntimeLib/Types/LanguageTypes/LanguageTypesFunc.h>
 #include <RuntimeLib/Evaluations/Expression/AdditiveOperators/AdditiveOperators.h>
 #include <RuntimeLib/Evaluations/Expression/MultiplicativeOperators/MultiplicativeOperators.h>
+#include <RuntimeLib/Types/SpecificationTypes/RecordType/RecordType.h>
 
 void _calculate(LanguageType *, string, LanguageType *);
+void _listItemsInRecord(RecordType *);
