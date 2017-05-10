fe8147b2612aefc098e1a59acf4805bb4135870f
John Tran <j23.tran@connect.qut.edu.au>
2017-05-11 02:24:21 +1000

    Update EnvironmentRecord items

--- RuntimeLib/Type/SpecificationType/Record/EnvironmentRecord/EnvironmentRecord.cpp
+++ RuntimeLib/Type/SpecificationType/Record/EnvironmentRecord/EnvironmentRecord.cpp
@@ -11,5 +11,5 @@ CompletionRecord* EnvironmentRecord::SetMutableBinding(StringType * N, LanguageT
 LanguageType* EnvironmentRecord::GetBindingValue(StringType* N, BooleanType* S) {
 
 	//Perform Binding
-	return new BooleanType(false);
+	return nullptr;
 }
