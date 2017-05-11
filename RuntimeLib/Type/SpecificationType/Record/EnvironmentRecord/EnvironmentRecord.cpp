db0b059546c18cc66e1e3a423b90580b721a18e3
John Tran <j23.tran@connect.qut.edu.au>
2017-05-11 12:28:09 +1000

    Notes for GetBindingValue

--- RuntimeLib/Type/SpecificationType/Record/EnvironmentRecord/EnvironmentRecord.cpp
+++ RuntimeLib/Type/SpecificationType/Record/EnvironmentRecord/EnvironmentRecord.cpp
@@ -9,15 +9,34 @@ CompletionRecord* EnvironmentRecord::SetMutableBinding(StringType * N, LanguageT
 	return NormalCompletion(nullptr);
 }
 LanguageType* EnvironmentRecord::GetBindingValue(StringType* N, BooleanType* S) {
+	//The concrete Environment Record method GetBindingValue for object Environment Records returns the value of its associated binding object's property whose name is the String value of the argument identifier N. The property should already exist but if it does not the result depends upon the value of the S argument:
 
-	//Perform Binding
+	//Let envRec be the object Environment Record for which the method was invoked.
+	//Let bindings be the binding object for envRec.
+	//Let value be ? HasProperty(bindings, N).
+	//If value is false, then
+	//If S is false, return the value undefined; otherwise throw a ReferenceError exception.
+	//Return ? Get(bindings, N).
+
+	auto envRec = this;
+	//auto bindings = bindingObject;
+	//auto value = HasProperty(binding, N);
+	//if (!value->_getValue()){
+	//	if (!S->_getValue())
+	//		return new UndefinedType();
+	//	else {
+	//		puts(ReferenceError);
+	//		exit(0);
+	//	}
+	//}
+	//return Get(bindings, N);
 	return new UndefinedType();
 }
 
 CompletionRecord * EnvironmentRecord::InitializeBinding(StringType * N, Type * V)
 {
 	//Let envRec be the declarative Environment Record for which the method was invoked.
-	auto envRec = new EnvironmentRecord();
+	auto envRec = this;
 	//Assert: envRec must have an uninitialized binding for N.
 	//Set the bound value for N in envRec to V.
 	//Record that the binding for N in envRec has been initialized.
