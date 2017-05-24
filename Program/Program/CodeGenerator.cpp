bcd0467232e48f57f5474c852327fc06019cf939
CemCC <cemcaliskan@ymail.com>
2017-05-24 23:36:58 +1000

    Code Generator was coded.

--- Program/Program/CodeGenerator.cpp
+++ Program/Program/CodeGenerator.cpp
@@ -14,8 +14,8 @@ int Counter = 0;
 
 void GeneratingCode(char* input, Script* root) {
 
-	char* outputFilename = (char*)malloc(strlen(inputfile) + 1);
-	sprintf(outputFilename, "%s.cpp", inputfile);
+	char* outputFilename = (char*)malloc(strlen(input) + 1);
+	sprintf(outputFilename, "%s.cpp", input);
 	FILE* outputFile = fopen(outputFilename, "generetedFile");
 
 	//Header Files
@@ -23,6 +23,27 @@ void GeneratingCode(char* input, Script* root) {
 	root->emit(outputFile, "#include \"RuntimeLib/RuntimeSemantic.h\"");
 	root->emit(outputFile, "#include \"RuntimeLib/Operation.h\"");
 	root->emit(outputFile, "#include <iostream>");
+	//Main function
 
+	root->emit(outputFile, "int main(int argc, char* argv[])");
+	root->emit(outputFile, "{");
 
+	//global environment
+
+	root->emit(outputFile, "LexicalEnvironment* lexEnv = NewDeclarativeEnvironment(nullptr);");
+	root->GenCode(outputFile);
+
+	//value logging
+
+	root->emit(outputFile, "std::cout << dynamic_cast<JSValue*>(r%d)->ToString();", Counter - 1);
+	root->emit(outputFile, "}");
+
+}
+
+int main(int argc, char* argv[]) {
+	fopen_s(&yyin, argv[1], "r");
+	yyparse();
+	root->dump(0);
+	GeneratingCode(argv[1], root);
+	getchar();
 }
