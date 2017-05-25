7dc6d49d9b38c4de7062752dde908781d7f3fe87
CemCC <cemcaliskan@ymail.com>
2017-05-25 18:41:15 +1000

    Rearranged File Structure

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 
 
+
 int yylex();
 int yyparse();
 
@@ -16,15 +17,43 @@ extern FILE *yyin;
 extern Script *root;
 
 
+int Counter = 0;
 
+void GeneratingCode(char* input, Script* root) {
+
+	char* outputFilename = (char*)malloc(strlen(input) + 1);
+	sprintf(outputFilename, "%s.cpp", input);
+	FILE* outputFile = fopen(outputFilename, "generetedFile");
+
+	//Header Files
+	root->emit(outputFile, "#include \"RuntimeLib/SpecificationType/LexicalEnvironment.h\"");
+	root->emit(outputFile, "#include \"RuntimeLib/RuntimeSemantic.h\"");
+	root->emit(outputFile, "#include \"RuntimeLib/Operation.h\"");
+	root->emit(outputFile, "#include <iostream>");
+	//Main function
+
+	root->emit(outputFile, "int main(int argc, char* argv[])");
+	root->emit(outputFile, "{");
+
+	//global environment
+
+	root->emit(outputFile, "LexicalEnvironment* lexEnv = NewDeclarativeEnvironment(nullptr);");
+	root->GenCode(outputFile);
+
+	//value logging
+
+	root->emit(outputFile, "std::cout << dynamic_cast<JSValue*>(r%d)->ToString();", Counter - 1);
+	root->emit(outputFile, "}");
+
+}
 int main(int argc, char* argv[]) {
 
-	fopen_s(&yyin, argv[1], "r");
+	/*fopen_s(&yyin, argv[1], "r");
 	yyparse();
 	root->dump(0);
-	getchar();
-	//
-	LexicalEnvironment* lexEnv = NewDeclarativeEnvironment(nullptr);
+	getchar();*/
+	
+	/*LexicalEnvironment* lexEnv = NewDeclarativeEnvironment(nullptr);
 
 	Type* r0 = ResolveBinding("x", lexEnv);
 	Type* r1 = new NumberValue(42);
@@ -36,4 +65,8 @@ int main(int argc, char* argv[]) {
 	Type* r7 = assignment(r3, r6);
 	std::cout << dynamic_cast<JSValue*>(r7)->ToString();
 	getchar();
+
+	GeneratingCode(argv[1], root);*/
+	puts("1");
 }
+
