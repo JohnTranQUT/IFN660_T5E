a0726cdfe76ab42d9f07f69ab2d9be50f1f465fe
John Tran <j23.tran@connect.qut.edu.au>
2017-05-29 16:38:45 +1000

    MultiplicativeOperators have Modulus and Division.

--- RuntimeLib/Expressions/MultiplicativeOperators.cpp
+++ RuntimeLib/Expressions/MultiplicativeOperators.cpp
@@ -19,5 +19,21 @@ LanguageType * Multiplicative(Type * left, Type * right, string _operator){
 			return new NumberType(-INFINITY);
 		return  new NumberType(lnum * rnum);
 	}
+	if (_operator == "/") {
+		if (lnum == NAN || rnum == NAN)
+			return new NumberType(NAN);
+		if (lnum == INFINITY && rnum == INFINITY)
+			return new NumberType(NAN);
+		if (lnum == INFINITY && rnum == 0)
+			return new NumberType(INFINITY);
+		if (lnum == -INFINITY && rnum == 0)
+			return new NumberType(-INFINITY);
+		/* stopped at 4th step */
+		return  new NumberType(lnum / rnum);
+	}
+	if (_operator == "%") {
+		/* skipped exceptions for now*/
+		return  new NumberType((int)round(lnum) % (int)round(rnum));
+	}
 	return  new NumberType(0);
 }
